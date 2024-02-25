38f417e58ada773a50d45165cbe773a5570c5f7c
Hugo Castel-Laville <hcastellaville@gmail.com>
2024-02-25 15:26:55 +0100

    replace os.listdir by os.walk

--- Backups/backup_core.py
+++ Backups/backup_core.py
@@ -21,7 +21,7 @@ class Backup:
         self.s_destination_path = s_destination_path
         self.s_date_format = "%Y%m%d%H%M%S"
         self.o_now = datetime.datetime.now()
-        self.l_dirs_to_backup_fp = []
+        self.l_files_to_backup_fp = []
         self.l_exclude = []
         self.o_logger = common.infradmin_logs.O_LOGGER
 
@@ -41,25 +41,24 @@ class Backup:
     def backup(self):
         self.o_logger.info("starting backup...")
         self.get_list_dirs_to_exclude()
-        for s_dir_to_backup in os.listdir(self.s_source_data):
-            s_dir_to_backup_fp = self.s_source_data + s_dir_to_backup
-            self.o_logger.info("adding " + s_dir_to_backup_fp + " in the backup list")
-            self.l_dirs_to_backup_fp.append(s_dir_to_backup_fp)
+        for s_dir_path, l_subdirectories, l_files in os.walk(self.s_source_data):
+            for s_file in l_files:
+                s_file_to_backup_fp = os.path.join(s_dir_path, s_file)
+                self.o_logger.info("adding " + s_file_to_backup_fp + " in the backup list")
+                self.l_files_to_backup_fp.append(s_file_to_backup_fp)
         for s_dir_to_exclude in self.l_exclude:
-            self.o_logger.info("--------------dir to exclude " + s_dir_to_exclude)
-            for s_dir_to_backup in self.l_dirs_to_backup_fp:
-                self.o_logger.info("--------------dir to bck " + s_dir_to_backup)
-                if s_dir_to_exclude in s_dir_to_backup:
-                    self.o_logger.info("deleting " + s_dir_to_exclude + " in the backup list")
-                    self.l_dirs_to_backup_fp.remove(s_dir_to_backup)
+            for s_file_to_backup in self.l_files_to_backup_fp:
+                if s_dir_to_exclude in s_file_to_backup:
+                    self.o_logger.info("deleting " + s_file_to_backup + " in the backup list")
+                    self.l_files_to_backup_fp.remove(s_file_to_backup)
 
         s_timestamp = self.o_now.strftime(self.s_date_format)
         s_backup_filename = s_timestamp + "-mars_backup.tar.gz"
         self.o_logger.info("backup name will be : " + s_backup_filename)
         o_tgz_file = tarfile.open(self.s_bck_path + s_backup_filename, 'w:gz')
-        for s_dir_fp in self.l_dirs_to_backup_fp:
-            self.o_logger.info("backuping : " + s_dir_fp)
-            o_tgz_file.add(s_dir_fp)
+        for s_file_fp in self.l_files_to_backup_fp:
+            self.o_logger.info("backuping : " + s_file_fp)
+            o_tgz_file.add(s_file_fp)
         o_tgz_file.close()
         self.rotate()
         self.push_backups()
