9ab2eb94cd121072c049a9973d8ef915cfc621b5
Hugo Castel-Laville <hcastellaville@gmail.com>
2024-02-25 14:47:19 +0100

    fix list to backup

--- Backups/backup_core.py
+++ Backups/backup_core.py
@@ -42,11 +42,13 @@ class Backup:
         self.o_logger.info("starting backup...")
         self.get_list_dirs_to_exclude()
         for s_dir_to_backup in os.listdir(self.s_source_data):
-            for s_dir_to_exclude in self.l_exclude:
-                if s_dir_to_exclude not in s_dir_to_backup:
-                    #s_dir_to_backup_fp = self.s_source_data + s_dir_to_backup
-                    self.o_logger.info("-----------------adding path : " + s_dir_to_backup)
-                    self.l_dirs_to_backup_fp.append(s_dir_to_backup)
+            s_dir_to_backup_fp = self.s_source_data + s_dir_to_backup
+            self.o_logger.info("adding " + s_dir_to_backup_fp + "in the backup list")
+            self.l_dirs_to_backup_fp.append(s_dir_to_backup_fp)
+        for s_dir_to_exclude in self.l_exclude:
+            if s_dir_to_exclude in self.l_dirs_to_backup_fp:
+                self.o_logger.info("deleting " + s_dir_to_exclude + "in the backup list")
+                self.l_dirs_to_backup_fp.remove(s_dir_to_exclude)
 
         s_timestamp = self.o_now.strftime(self.s_date_format)
         s_backup_filename = s_timestamp + "-mars_backup.tar.gz"
