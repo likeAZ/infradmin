e1282f7fb6cff3eee46d919f18f210ba6575d645
Hugo Castel Laville <hugo.castellaville.ext@orange.com>
2024-02-29 13:38:24 +0000

    bck 20240229

--- Backups/backup_core.py
+++ Backups/backup_core.py
@@ -12,7 +12,8 @@ class Backup:
         #with open("/usr/src/app/infradmin/conf/containers.json", 'r') as fp:
         #    self.d_json_conf = json.load(fp)
         #fp.close()
-        self.s_exclude_conf = "/usr/src/app/infradmin/conf/backup/backup.yaml"
+        self.s_conf_file = "/usr/src/app/infradmin/conf/backup/backup.yaml"
+        self.s_exclude_conf = "/usr/src/app/infradmin/conf/backup/exclude.yaml"
         self.i_keep = i_keep
         self.s_bck_path = "/usr/src/app/infradmin/backup/"
         self.s_source_data = "/usr/src/app/infradmin/data/"
@@ -22,7 +23,9 @@ class Backup:
         self.s_date_format = "%Y%m%d%H%M%S"
         self.o_now = datetime.datetime.now()
         self.l_files_to_backup_fp = []
-        self.l_exclude = []
+        self.l_backup_name = []
+        self.d_yaml = []
+        self.s_backup_filename = None
         self.o_logger = common.infradmin_logs.O_LOGGER
 
     def run_command(self, command=None):
@@ -30,46 +33,69 @@ class Backup:
 
     def backups(self):
         self.get_info_from_conf()
+        self.do_backup()
+        for s_backup_name in self.l_backup_name:
+            if self.is_local(s_backup_name):
+                self.rotate(self.get_retention(s_backup_name))
+            else:
+                self.rotate(self.get_retention(s_backup_name))
+                self.push_backups()
 
     def get_info_from_conf(self):
         self.o_logger.info("getting exclude list")
-        with open(self.s_exclude_conf) as o_yaml_conf_file:
-            o_yaml = yaml.safe_load(o_yaml_conf_file)
-        self.l_exclude = o_yaml['exclude']
+        with open(self.s_conf_file) as o_yaml_conf_file:
+            self.d_yaml = yaml.safe_load(o_yaml_conf_file)
+        self.l_backup_name = list(self.d_yaml.keys())
         o_yaml_conf_file.close()
-        self.o_logger.info("exclude list is :")
-        for s_exclude_dir in self.l_exclude:
+
+    def get_exclude_list(self):
+        o_exclude_file = yaml.safe_load(self.s_exclude_conf)
+        l_exclude = o_exclude_file['exclude']
+        self.o_logger.info("exclude list for is :")
+        for s_exclude_dir in l_exclude:
             self.o_logger.info(s_exclude_dir)
+        return l_exclude
+
+    def is_local(self, s_backup_name):
+        b_is_local = False
+        if self.d_yaml[s_backup_name]['type'] == 'local':
+            b_is_local = True
+        return b_is_local
+
+    def delete_local_backup(self):
+        os.remove(self.s_bck_path + self.s_backup_filename)
+
+    def get_retention(self, s_backup_name):
+        return self.d_yaml[s_backup_name]['keep']
 
     def do_backup(self):
         self.o_logger.info("starting backup...")
         l_files_to_backup_fp_tmp = []
+        l_exclude = self.get_exclude_list()
         for s_dir_path, l_subdirectories, l_files in os.walk(self.s_source_data):
             for s_file in l_files:
                 s_file_to_backup_fp = os.path.join(s_dir_path, s_file)
                 l_files_to_backup_fp_tmp.append(s_file_to_backup_fp)
-        for s_dir_to_exclude in self.l_exclude:
+        for s_dir_to_exclude in l_exclude:
             self.o_logger.info("deleting " + s_dir_to_exclude + " in the backup list")
             l_files_to_backup_fp_tmp = [file_path for file_path in l_files_to_backup_fp_tmp if not file_path.startswith(s_dir_to_exclude)]
         self.l_files_to_backup_fp = l_files_to_backup_fp_tmp
         s_timestamp = self.o_now.strftime(self.s_date_format)
-        s_backup_filename = s_timestamp + "-mars_backup.tar.gz"
-        self.o_logger.info("backup name will be : " + s_backup_filename)
-        o_tgz_file = tarfile.open(self.s_bck_path + s_backup_filename, 'w:gz')
+        self.s_backup_filename = s_timestamp + "-mars.tar.gz"
+        self.o_logger.info("backup name will be : " + self.s_backup_filename)
+        o_tgz_file = tarfile.open(self.s_bck_path + self.s_backup_filename, 'w:gz')
         for s_file_fp in self.l_files_to_backup_fp:
             self.o_logger.info("backuping : " + s_file_fp)
             o_tgz_file.add(s_file_fp)
         o_tgz_file.close()
-        self.rotate()
-        self.push_backups()
 
-    def rotate(self):
-        self.o_logger.info("deleting files older than : " + str(self.i_keep) + "days")
+    def rotate(self, i_keep):
+        self.o_logger.info("deleting files older than : " + str(i_keep) + "days")
         for s_dir_backup in os.listdir(self.s_bck_path):
             s_date_backup = s_dir_backup[:8]
             o_date_backup = datetime.datetime.strptime(s_date_backup, self.s_date_format[:8])
             i_date_delta = int((self.o_now - o_date_backup).days)
-            if i_date_delta >= self.i_keep:
+            if i_date_delta >= i_keep:
                 os.remove(self.s_bck_path + s_dir_backup)
 
     def push_backups(self):
