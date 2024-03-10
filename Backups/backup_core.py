a0abfb6b1c6e18b5135b3acee0d0e71ba4b4700d
Hugo Castel-Laville <hcastellaville@gmail.com>
2024-03-10 15:02:09 +0100

    add multi threads

--- Backups/backup_core.py
+++ Backups/backup_core.py
@@ -1,10 +1,12 @@
 import datetime
+import time
 import yaml
 import subprocess
 import os
 import tarfile
 import common.infradmin_logs
 import common.sftp
+import threading
 
 
 class Backup:
@@ -134,6 +136,20 @@ class Backup:
         """
         return self.d_yaml[s_backup_name]['backup_path']
 
+    def log_progress(self, period, total_files, progress_lock, progress):
+        while True:
+            time.sleep(period)
+            # Logger la progression
+            with progress_lock:
+                self.o_logger.info(f"Progression: {progress[0]}/{total_files} fichiers traités.")
+
+    def add_to_tar(self, tar, file_list, progress_lock, progress):
+        for file in file_list:
+            tar.add(file)
+            # Incrémenter le compteur de progression
+            with progress_lock:
+                progress[0] += 1
+
     def do_backup(self):
         """
         Create a tar.gz file without dirs to exclude
@@ -144,25 +160,57 @@ class Backup:
         l_files_to_backup_fp_tmp = []
         l_exclude = self.get_exclude_list()
 
-        for s_dir_path, l_subdirectories, l_files in os.walk(self.s_source_data):
-            for s_file in l_files:
-                s_file_to_backup_fp = os.path.join(s_dir_path, s_file)
-                l_files_to_backup_fp_tmp.append(s_file_to_backup_fp)
+        def generate_files_to_backup():
+            for s_dir_path, _, l_files in os.walk(self.s_source_data):
+                for s_file in l_files:
+                    s_file_to_backup_fp = os.path.join(s_dir_path, s_file)
+                    if not any(s_file_to_backup_fp.startswith(s_dir) for s_dir in l_exclude):
+                        yield s_file_to_backup_fp
 
-        for s_dir_to_exclude in l_exclude:
-            self.o_logger.info("deleting " + s_dir_to_exclude + " in the backup list")
-            l_files_to_backup_fp_tmp = [file_path for file_path in l_files_to_backup_fp_tmp if not file_path.startswith(s_dir_to_exclude)]
-        self.l_files_to_backup_fp = l_files_to_backup_fp_tmp
+        self.l_files_to_backup_fp = list(generate_files_to_backup())
 
         s_timestamp = self.o_now.strftime(self.s_date_format)
         self.s_backup_filename = s_timestamp + "-mars.tar.gz"
         self.o_logger.info("backup name will be : " + self.s_backup_filename)
 
-        o_tgz_file = tarfile.open(self.s_bck_path + self.s_backup_filename, 'w:gz')
-        for s_file_fp in self.l_files_to_backup_fp:
-            self.o_logger.info("backuping : " + s_file_fp)
-            o_tgz_file.add(s_file_fp)
-        o_tgz_file.close()
+        with tarfile.open(self.s_bck_path + self.s_backup_filename, 'w:gz') as o_tgz_file:
+            num_threads = 3  # You can adjust the number of threads as needed
+            chunk_size = len(self.l_files_to_backup_fp) // num_threads
+            threads = []
+
+            # Initialisation de la progression
+            progress_lock = threading.Lock()
+            progress = [0]
+
+            # Démarrage du thread pour logger la progression
+            log_thread = threading.Thread(target=self.log_progress, args=(
+                30,
+                len(self.l_files_to_backup_fp),
+                progress_lock,
+                progress
+            ))
+            log_thread.start()
+
+            for i in range(num_threads):
+                start_index = i * chunk_size
+                end_index = start_index + chunk_size if i < num_threads - 1 else len(self.l_files_to_backup_fp)
+                thread = threading.Thread(target=self.add_to_tar,
+                                          args=(
+                                              o_tgz_file,
+                                              self.l_files_to_backup_fp[start_index:end_index],
+                                              progress_lock,
+                                              progress
+                                          ))
+                thread.start()
+                threads.append(thread)
+
+            # Attendre la fin de tous les threads
+            for thread in threads:
+                thread.join()
+
+            # Attendre la fin du thread de logging
+            log_thread.join()
+
         self.o_logger.info("Backup finished")
 
 
