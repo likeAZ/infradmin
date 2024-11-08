8498312a35b5d4180b779b5ddcfa20469316e3cd
likeAZ <az.integer@gmail.com>
2024-11-08 11:50:58 +0100

    fix volume

--- common/containers.py
+++ common/containers.py
@@ -77,14 +77,11 @@ class Docker:
         self.o_logger.info(f'volumes for {o_own_container.name} are : {l_own_container_volumes}')
         s_own_container_volume_source = None
         for s_own_container_volume in l_own_container_volumes:
-            self.o_logger.info(f'volume : {s_own_container_volume}')
-            self.o_logger.info(f'volume source : {s_own_container_volume.split(":")[0]}')
-            self.o_logger.info(f'volume destination : {s_own_container_volume.split(":")[1]}')
-            if s_own_container_volume.split(':')[1] == '/usr/src/app/infradmin/data/':
+            if s_own_container_volume.split(':')[1] == '/usr/src/app/infradmin/data':
                 s_own_container_volume_source = s_own_container_volume.split(':')[0]
                 break
         if s_own_container_volume_source is None:
-            raise ValueError("Volume source for not found")
+            self.o_logger.error("Volume source for /usr/src/app/infradmin/data/ not found")
         return s_own_container_volume_source
     
     def get_volumes_for_container(self, s_container_name: str) -> list:
