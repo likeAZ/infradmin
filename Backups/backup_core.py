d27231ecff02ca04d47eec4818f1a127bfa97315
likeAZ <az.integer@gmail.com>
2024-11-08 13:38:40 +0100

    fix pg_dump

--- Backups/backup_core.py
+++ Backups/backup_core.py
@@ -309,9 +309,15 @@ class Backup:
             s_backup_path_container_side = s_database_backup_volume.split(":")[1]
             s_backup_path_host_side = s_database_backup_volume.split(":")[0]
             
-            if os.path.exists(self.map_volume_path(s_backup_path_host_side)+"*"):
-                self.o_logger.info("Deleting old database dumps")
-                os.remove(self.map_volume_path(s_backup_path_host_side) + "*")
+            if os.path.exists(self.map_volume_path(s_backup_path_host_side)):
+                if os.listdir(self.map_volume_path(s_backup_path_host_side)):
+                    self.o_logger.info("Deleting old database dumps")
+                    for s_file in os.listdir(self.map_volume_path(s_backup_path_host_side)):
+                        os.remove(os.path.join(self.map_volume_path(s_backup_path_host_side), s_file))
+                else:
+                    self.o_logger.info("No old database dumps to delete")
+            else:
+                self.o_logger.info(f"Directory {self.map_volume_path(s_backup_path_host_side)} does not exist")
             
             if 'nextcloud_container_name' in self.d_yaml_databases[s_database_container_name] and self.o_docker.is_container_exist(self.d_yaml_databases[s_database_container_name]['nextcloud_container_name']):
                 self.o_logger.info(f"Putting {self.d_yaml_databases[s_database_container_name]['nextcloud_container_name']} in maintenance mode")
@@ -345,13 +351,13 @@ class Backup:
                         self.o_logger.info(f"Wainting for {s_database_container_name} to be started")
                         time.sleep(10)
                     
-                    s_backup_cmd = f"/usr/local/bin/pg_dumpall -U postgres > {s_backup_path_container_side}{datetime.datetime.now().strftime(self.s_date_format)}-backup.sql"
+                    s_backup_cmd = f"/bin/sh -c '/usr/local/bin/pg_dumpall -U $POSTGRES_USER > {s_backup_path_container_side}{datetime.datetime.now().strftime(self.s_date_format)}-backup.sql'"
                     self.o_docker.exec_command(self.o_docker.from_name_to_id(s_database_container_name), s_backup_cmd)
                     
                     self.o_logger.info(f"{s_database_container_name} backuped in {self.map_volume_path(s_backup_path_host_side)}")
                     
                     if s_previous_backup_state == 'exited':
-                        self.log.info(f"Returning {s_database_container_name} to previous state")
+                        self.o_logger.info(f"Returning {s_database_container_name} to previous state")
                         self.o_docker.stop_container(s_database_container_name)
                         
                 case 'unknown':
