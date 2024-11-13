8538ccd2fddadb9c9c8f01bf6ee2a9f20dbffc14
likeAZ <az.integer@gmail.com>
2024-11-13 12:02:25 +0100

    fixing script

--- scripts/charges.py
+++ scripts/charges.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import yaml
+import argparse
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
@@ -17,15 +18,21 @@ def calculate_transferable_amount(salary, charges):
     return transferable_amount
 
 def main():
+    parser = argparse.ArgumentParser(description='Calculate transferable amount after charges.')
+    parser.add_argument('salary', type=float, help='Salary in euros')
+    args = parser.parse_args()
+    
     common.infradmin_logs.O_LOGGER = common.infradmin_logs.init_logging('Transfer', False)
     yaml_file_path = '/usr/src/app/infradmin/conf/charges.yaml'
     charges = load_charges(yaml_file_path)
     
-    salary = float(input("Enter your salary in euros: "))
+    salary = args.salary
     transferable_amount = calculate_transferable_amount(salary, charges)
     
     common.infradmin_logs.O_LOGGER.info(f"Salary: {salary} euros")
-    common.infradmin_logs.O_LOGGER.info(f"Charges: {charges}")
+    common.infradmin_logs.O_LOGGER.info("Charges: ")
+    for charge, amount in charges.items():
+        common.infradmin_logs.O_LOGGER.info(f"{charge}: {amount} euros")
     common.infradmin_logs.O_LOGGER.info(f"You can transfer {transferable_amount:.2f} euros to another bank account.")
     
 if __name__ == "__main__":
